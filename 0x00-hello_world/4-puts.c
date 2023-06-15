3bc6de17d92a4aebab49c096a2bd2ddd53a87255
Maurine <maurinek04@gmail.com>
2023-06-15 17:49:45 +0000

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Using puts to print string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
