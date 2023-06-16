c2d1be38b9f2f6ab4e113071fd6e8d63bd6ee41d
Maurine <maurinek04@gmail.com>
2023-06-16 13:32:53 +0000

    Positive and negative variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - Prints if number is positive or negative
  *
@@ -12,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("if the number is greater than 0: is positive\n")
+printf("if the number is greater than 0: is positive\n");
 }
 if (n == 0)
 {
-printf("if the number is 0: is zero\n")
+printf("if the number is 0: is zero\n");
 }
 if (n < 0)
 {
-printf("if the number is less than 0: is negative\n")
+printf("if the number is less than 0: is negative\n");
 }
 return (0);
 }
