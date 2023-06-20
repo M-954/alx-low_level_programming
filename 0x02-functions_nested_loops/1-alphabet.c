da8d5bd8c80c2b7085771aad977bca9e5f844c71
Maurine <maurinek04@gmail.com>
2023-06-20 17:56:47 +0000

    lowercase letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0 Always(success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+/**
+ * print_alphabet - prints lowercase letters
+ */
+void print_alphabet(void)
+{
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+}
