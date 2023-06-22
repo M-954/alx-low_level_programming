be993a4fd20f3c3f8aba6c74f62bb8a0b6059765
Maurine <maurinek04@gmail.com>
2023-06-22 22:41:18 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	int z;
+
+	z = 1;
+
+	while (z <= 100)
+	{
+		if (z % 3 == 0 && z % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (z % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (z % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", z);
+		}
+		z++;
+	}
+	printf("\n");
+	return (0);
+}
