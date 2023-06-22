3ff4702f4756f283c3abc1b520d48b1f18a693f6
Maurine <maurinek04@gmail.com>
2023-06-22 20:46:43 +0000

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: size of square
+ */
+void print_square(int size)
+{
+	int a, d;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (d = 0; d < size; d++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
+
