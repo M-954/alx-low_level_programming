8ac5799eb24a437a06a657bbd06936b697aa2254
Maurine <maurinek04@gmail.com>
2023-07-04 08:01:40 +0000

    set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - modifies value of given string
+ * @s: pointer to string to be modified
+ * @to: pointer to new string to be set
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
