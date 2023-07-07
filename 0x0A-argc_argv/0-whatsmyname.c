fef0966fab1ebfb92d0b2fb0743b0f8279948ad3
Maurine <maurinek04@gmail.com>
2023-07-07 22:26:56 +0000

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
