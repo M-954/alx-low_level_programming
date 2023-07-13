761444fe1a55a576bf5d6bdf1cde4b85aaebb542
Maurine <maurinek04@gmail.com>
2023-07-13 21:22:14 +0000

    memory allocation for array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	p = malloc((sizeof(int) * (max - min)) + sizeof(int));
+	p = malloc(sizeof(int) * (max - min + 1) + sizeof(int));
 
 	for (a = 0; min <= max; a++)
 	{
