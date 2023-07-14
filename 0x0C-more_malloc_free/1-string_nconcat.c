fdfdcf666bd8ad917f286d2849b259c4e76eadec
Maurine <maurinek04@gmail.com>
2023-07-14 18:04:51 +0000

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = len2;
 	}
 
-	p = (char *)malloc(sizeof(char) * (len1 + n) + 1);
+	p = (char *)malloc(sizeof(char) * (len1 + n + 1));
 
 	strcpy(p, s1);
 
