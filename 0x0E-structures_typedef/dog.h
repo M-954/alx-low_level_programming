eb4421ce82f9fba646efe0065fd251a47d0db64d
Maurine <maurinek04@gmail.com>
2023-07-17 23:00:03 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - name of stuct
+ * @name: holds the name
+ * @age: holds the age
+ * @owner: holds the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
