bc2c28d6ba14999266636d6cbd6f0e5e7d5f0463
Maurine <maurinek04@gmail.com>
2023-07-27 22:17:24 +0000

    prints all elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements of a list
+ * @h: pointer to head of list
+ *
+ * Return: number of nodes in list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t c = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		c++;
+	}
+	return (c);
+}
