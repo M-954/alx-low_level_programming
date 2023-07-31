ef8ad7669d8aee984f7ede504cc3acd63c0444f4
Maurine <maurinek04@gmail.com>
2023-07-31 17:35:34 +0000

    prints all elements in a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,11 +7,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
+		h = h->next;
 		i++;
 	}
 	return (i);
