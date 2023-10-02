fee91631c142c28b887467ef7136865fe1897732
Maurine <maurinek04@gmail.com>
2023-10-02 23:17:52 +0000

    function that reads a text file and prints it to the standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it out
+ * @filename: pointer to a filename
+ * @letters: number of characters to print out
+ *
+ * Return: number of characters to print out
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *p;
+	char *buf;
+	ssize_t readfile, writefile;
+
+	p = fopen(filename, "r");
+	if (p == NULL)
+	{
+		return (0);
+	}
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		fclose(p);
+		return (0);
+	}
+	readfile = fread(buf, sizeof(char), letters, p);
+	if (readfile == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	writefile = write(STDOUT_FILENO, buf, readfile);
+	free(buf);
+	fclose(p);
+	if (writefile != readfile)
+	{
+		return (0);
+	}
+	return (writefile);
+}
