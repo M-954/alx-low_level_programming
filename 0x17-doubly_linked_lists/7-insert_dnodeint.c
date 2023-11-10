8b3ad188162cae373778c94ea8ee5bdc5dc2c5ba
Maurine <maurinek04@gmail.com>
2023-11-10 04:38:05 +0000

    inserts node at specified index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,6 +4,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *newnode, *temp;
 	unsigned int count = 0;
 
+	if (*h == NULL)
+		return (NULL);
 	temp = *h;
 	newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
@@ -21,19 +23,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		count++;
 	}
-	newnode->next = temp->next;
-	if (temp != NULL)
-	{
-		temp->next->prev = newnode;
-		newnode->prev = temp;
-		newnode->n = n;
-		temp->next = newnode;
-	}
-
 	if (temp == NULL)
 	{
 		free(newnode);
 		return (NULL);
 	}
+	newnode->next = temp->next;
+	temp->next->prev = newnode;
+	newnode->prev = temp;
+	newnode->n = n;
+	temp->next = newnode;
 	return (newnode);
 }
