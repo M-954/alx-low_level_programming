4143c4bf04575e654c42dc79cd5215a00ab693f0
Maurine <maurinek04@gmail.com>
2023-11-10 06:16:28 +0000

    inserts node at given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,6 +29,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	newnode->next = temp->next;
+	if (temp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}	
 	if (temp->next != NULL)
 		temp->next->prev = newnode;
 	newnode->prev = temp;
