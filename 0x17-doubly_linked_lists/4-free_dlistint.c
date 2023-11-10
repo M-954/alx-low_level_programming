a16341e3ed6823593f7630b79b4b218240b571c4
Maurine <maurinek04@gmail.com>
2023-11-10 02:17:01 +0000

    frees dynamically allocated memory

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a doubly linked list
+ * @h: pointer to header
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return;
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
