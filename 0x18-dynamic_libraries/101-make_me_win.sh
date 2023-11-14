1cfebf933d34ece2bc5ad8d891732ec06933b672
Maurine <maurinek04@gmail.com>
2023-11-14 14:00:26 +0000

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/M-954/alx-low_level_programming/master/0x18-dynamic_libraries/libinjection.so
+wget -P .. \
+	https://raw.githubusercontent.com/M-954/alx-low_level_programming/master/0x18-dynamic_libraries/libinjection.so
 export LD_PRELOAD="$PWD/../libinjection.so"
